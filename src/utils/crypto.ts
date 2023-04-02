10185b6c8c2f9379b42d22c0328c078de123b854
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-04-02 23:48:14 +0200

    Update crypto.ts

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,3 +1,3 @@
 import { createHash as cryptoCreateHash, type Hash } from 'node:crypto';
-
+console.error('deprecated: ', import.meta.url, 'Use Web Crypto crypto.webcrypto.subtile'  )
 export const createHash = (): Hash => cryptoCreateHash('sha1');
