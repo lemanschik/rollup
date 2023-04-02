a14f62225d27618a7ce28bad36c0277afaf22828
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-04-02 23:44:07 +0200

    Update crypto.ts
    easy change to sha-1 because git uses that for object hashes.

--- browser/src/crypto.ts
+++ browser/src/crypto.ts
@@ -1,6 +1,6 @@
-import sha256 from 'hash.js/lib/hash/sha/256';
+import sha1 from 'hash.js/lib/hash/sha/1.js';
 
 export const createHash = (): {
 	digest: (format: string) => string;
 	update: (data: unknown) => void;
-} => sha256();
+} => sha1();
