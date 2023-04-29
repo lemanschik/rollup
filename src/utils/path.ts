4bbf8ca9815bc679aaae329eb7e4b54708fdc974
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-04-29 13:56:48 +0200

    Update path.ts

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -13,4 +13,17 @@ export function normalize(path: string) {
 	return path.replace(/\\/g, '/');
 }
 
-export { basename, dirname, extname, relative, resolve } from 'path';
+export function extname(path: string) {
+	return path.slice(path.lastIndexOf('.'))
+}
+
+export function basename(path: string, ext) {
+	return path.slice(0,path.lastIndexOf(ext || extname(path))
+}
+
+export function dirname(path: string) {
+	const splitResult = path.split('/');
+	splitResult[splitResult.length-1];
+}
+
+export { relative, resolve } from 'node-path.js';
